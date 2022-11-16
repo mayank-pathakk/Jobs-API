72bf9225708c30649801b922b8cb99ff75ef0e69
bad-at-names0 <mayank234pathak@gmail.com>
2022-11-16 14:33:03 +0530

    Distinguish users

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -1,11 +1,20 @@
+const Job = require("../models/Job");
+const { BadRequestError } = require("../errors");
+const notFound = require("../middleware/not-found");
+const { StatusCodes } = require("http-status-codes");
+
 const getAllJobs = async (req, res) => {
-  res.send("Get all the jobs");
+  const userId = req.user.userId
+  const jobs = await Job.find({createdBy : userId}).sort("createdAt")
+  res.status(StatusCodes.OK).json({jobs , count : jobs.length})
 };
 const getJob = async (req, res) => {
-  res.send("Get job");
+  res.send("Get job"); 
 };
 const createJob = async (req, res) => {
-  res.send("Create job");
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 const deleteJob = async (req, res) => {
   res.send("Delete job");
