d1455e3e33f04a7ae5e648cb522c7f4cd4928589
bad-at-names0 <mayank234pathak@gmail.com>
2022-11-21 21:22:48 +0530

    Daily work

--- starter/routes/jobs.js
+++ starter/routes/jobs.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getAllJobs,
-    getJob,
-    createJob,
-    deleteJob,
-    updateJob,
-} = require('../controllers/jobs')
+  getJob,
+  getAllJobs,
+  createJob,
+  deleteJob,
+  updateJob,
+} = require("../controllers/jobs");
 
-router.route('/').get(getAllJobs).post(createJob)
-router.route('/:id').delete(deleteJob).patch(updateJob).get(getJob)
+router.route("/").get(getAllJobs).post(createJob);
+router.route("/:id").delete(deleteJob).patch(updateJob).get(getJob);
 
-module.exports = router
+module.exports = router;
