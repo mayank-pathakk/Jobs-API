53de003e4e615347fc90fca4c3f06134aef68cb6
bad-at-names0 <mayank234pathak@gmail.com>
2022-12-04 09:34:08 +0530

    Message edit

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    message: err.message || `Something went wrong , try again later...`,
+    message: err.message || `Something went wrong ,please try again later...`,
   };
 
   if (err.name === "ValidationError") {
