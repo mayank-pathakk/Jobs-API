17a7fb5b31b8b914ecdf9be13f9aa705718abcdb
bad-at-names0 <mayank234pathak@gmail.com>
2022-12-08 09:44:52 +0530

    Minor spelling bugs

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -11,26 +11,25 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    throw new BadRequestError("Please provide email and password");
+    throw new BadRequestError("Please provide email or password");
   }
 
   const user = await User.findOne({ email });
 
-  
   if (!user) {
     throw new UnauthenticatedError("Invalid credentials");
   }
-  
+
   // check password
-  const isPasswordCorrect = await user.comparePassword(password)
-  if(!isPasswordCorrect) {
-    throw new UnauthenticatedError("Invalid username or password")
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid username or password");
   }
 
   const token = user.createJWT();
 
   res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
-}; 
+};
 
 module.exports = {
   register,
